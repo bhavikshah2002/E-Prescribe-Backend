e8245d9f37e9bcdc93f7aca892316910f16555fe
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-06-16 22:02:28 +0530

    SwaggerFixing2

--- doctorsBay/urls.py
+++ doctorsBay/urls.py
@@ -37,5 +37,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
