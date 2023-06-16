f29496ace537836dc001ce6a6e6854a966461b5e
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-06-16 22:00:06 +0530

    SwaggerFixing

--- doctorsBay/urls.py
+++ doctorsBay/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import permissions
 from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -28,6 +29,7 @@ schema_view = get_schema_view(
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
+      permission_classes=[permissions.AllowAny],
    ),
    public=True,
 )
