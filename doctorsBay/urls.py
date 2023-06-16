ce5c403847fd47ebc7e635bc9ea1b03c59d453fd
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-06-16 22:28:15 +0530

    Final Swagger

--- doctorsBay/urls.py
+++ doctorsBay/urls.py
@@ -6,9 +6,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Gitbase API",
+      title="Doctor's Bay API",
       default_version='v1',
-      description="A place to store files",
+      description="E-Prescription Project",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
