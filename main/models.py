ef27ffd891b75b7b5d7cdebe9f48ce0a31547dfb
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-06-22 21:45:48 +0530

    Visit Model Work Done

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import MyUser
+import json
 # Create your models here.
 
 class Session(models.Model):
@@ -10,4 +11,20 @@ class Session(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.session_id)
+
+class Vist(models.Model):
+    visit_id = models.AutoField(primary_key=True)
+    visit_date = models.DateTimeField(auto_now_add=True)
+    symptoms = models.CharField(max_length=250,blank=True,null=True)
+    note = models.CharField(max_length=500,blank=True,null=True)
+    temperature = models.IntegerField(null=True,blank=True)
+    sugar = models.IntegerField(null=True,blank=True)
+    bp = models.CharField(null=True,blank=True,max_length=10)
+    def set_symptoms(self, x):
+        self.symptoms = json.dumps(x)
+
+    def get_symptoms(self):
+        return json.loads(self.symptoms)
     
+    def __str__(self):
+        return str(self.visit_id)
